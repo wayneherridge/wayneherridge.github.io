8d24fd360937e69e0daa85fa6fc94c73935ff7ba
wayneherridge <wsherridge@icloud.com>
2019-02-11 23:24:38 +0000

    changed the date code i js

--- weather6/scripts/currentdate.js
+++ weather6/scripts/currentdate.js
@@ -1,7 +1,7 @@
 function currentDate() {
     var d = new Date();
 var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-document.getElementById("demo").innerHTML = days[d.getDay()];
-document.getElementById("demo").innerHTML = d.getMonth();
-document.getElementById("demo").innerHTML = d.getFullYear();
+document.getElementById("day").innerHTML = days[d.getDay()];
+document.getElementById("month").innerHTML = d.getMonth();
+document.getElementById("year").innerHTML = d.getFullYear();
 }
