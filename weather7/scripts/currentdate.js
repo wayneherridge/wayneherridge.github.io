4df6f23e0b7a4ebba9f012394f7c8d4a1d46f033
wayneherridge <wsherridge@icloud.com>
2019-02-18 18:27:42 +0000

    amended files

---
+++ weather7/scripts/currentdate.js
@@ -0,0 +1,35 @@
+function myFunction() {
+  var d = new Date();
+  var day = d.getDate();
+  
+  var weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+
+  var n = weekday[d.getDay()];
+  
+   var month = new Array();
+  month[0] = "January";
+  month[1] = "February";
+  month[2] = "March";
+  month[3] = "April";
+  month[4] = "May";
+  month[5] = "June";
+  month[6] = "July";
+  month[7] = "August";
+  month[8] = "September";
+  month[9] = "October";
+  month[10] = "November";
+  month[11] = "December";
+  var mon = month[d.getMonth()];
+  
+  var yr = d.getFullYear();  
+  
+  
+  document.getElementById("currentDate").innerHTML = n + ", " + day + " " + mon + " " + yr;
+}
