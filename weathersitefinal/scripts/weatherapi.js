8eb3beec74531a326f9fc63e2009c502c2d16a5e
wayneherridge <wsherridge@icloud.com>
2019-03-23 00:06:38 +0000

    removed http: for github

--- weathersitefinal/scripts/weatherapi.js
+++ weathersitefinal/scripts/weatherapi.js
@@ -1,7 +1,7 @@
     
     let weatherRequest = new XMLHttpRequest();
 
-weatherRequest.open('GET','http://api.openweathermap.org/data/2.5/weather?id=5061036&appid=2be184dbacae081dacf9bde2487c043c&units=imperial', true);
+weatherRequest.open('GET','//api.openweathermap.org/data/2.5/weather?id=5061036&appid=2be184dbacae081dacf9bde2487c043c&units=imperial', true);
 
 weatherRequest.send();
 
@@ -22,7 +22,7 @@ weatherRequest.onload = function() {
     var windSpeed = parseFloat(document.getElementById('windSpeed').innerHTML = weatherData.wind.speed);
     
     var iconcode = weatherData.weather[0].icon;
-    var icon_path = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    var icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
     document.getElementById('weather_icon').src = icon_path;
   
   var temp = (lowTemp + highTemp) / 2;
