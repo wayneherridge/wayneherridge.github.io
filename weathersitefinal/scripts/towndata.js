59f1a946d25f0dbbb327bac495eff0053f57ef3c
wayneherridge <wsherridge@icloud.com>
2019-03-23 00:12:09 +0000

    removed http:

--- weathersitefinal/scripts/towndata.js
+++ weathersitefinal/scripts/towndata.js
@@ -1,5 +1,5 @@
 var section = document.querySelector('section');
-var requestURL = 'http://byui-cit230.github.io/weather/data/towndata.json';
+var requestURL = '//byui-cit230.github.io/weather/data/towndata.json';
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 
