75088b0d560edca9b3ab542be4ef93fe776464ae
wayneherridge <wsherridge@icloud.com>
2019-03-29 21:18:53 +0000

    added and edited files for the site

---
+++ templesitefinal/js/script.js
@@ -0,0 +1,23 @@
+/* Current date calculation for the footer */
+
+function calcDate() { 
+  
+var d = new Date(); var day = d.getDate();
+  
+  var weekday = new Array(7); weekday[0] = "Sunday"; weekday[1] = "Monday"; weekday[2] = "Tuesday"; weekday[3] = "Wednesday"; weekday[4] = "Thursday"; weekday[5] = "Friday"; weekday[6] = "Saturday";
+
+  var n = weekday[d.getDay()];
+  
+  var month = new Array(); month[0] = "January"; month[1] = "February"; month[2] = "March"; month[3] = "April"; month[4] = "May"; month[5] = "June"; month[6] = "July"; month[7] = "August"; month[8] = "September"; month[9] = "October"; month[10] = "November"; month[11] = "December";
+   
+  var mon = month[d.getMonth()]; var yr=d.getFullYear();  
+  document.getElementById("currentDate").innerHTML = n + ", " + day + " " + mon + " " + yr;
+}
+calcDate();
+
+
+// Toggle the hamburger menu on and off.
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+
+}
